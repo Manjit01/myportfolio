83d4cc12af3bea1cd9b915e39211c01aa3e81428
Manjit Kaur <bajwa36@outlook.com>
2020-09-09 09:25:20 +1200

    Updates

---
+++ precache-manifest.fde1fa027d58f016190b5ff12c47e4e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b1de03e504ec0fa910a1f9136903222",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "45239dca2259328410ab",
+    "url": "/myportfolio/static/css/main.5c2ae854.chunk.css"
+  },
+  {
+    "revision": "97e2b2f0fa24a62c6baa",
+    "url": "/myportfolio/static/js/2.b116c5ec.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/myportfolio/static/js/2.b116c5ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45239dca2259328410ab",
+    "url": "/myportfolio/static/js/main.6505be29.chunk.js"
+  },
+  {
+    "revision": "da2e02db1e870a8bb8e5",
+    "url": "/myportfolio/static/js/runtime-main.b5f0a273.js"
+  }
+]);
